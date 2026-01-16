051bf96cb96ecdc7b8699bdc59c39c49dd20394b
CICD-Company <jacopo.stucchi1@studenti.unimi.it>
2026-01-16 17:29:08 +0100

    fix log message double [LOG] string

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main():
     print("2 + 3 =", add(2, 3))
 
     # Function of the shared library
-    log("[LOG] Calculator started")
+    log("Calculator started")
 
 
 if __name__ == "__main__":
